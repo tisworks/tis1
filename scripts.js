33f666b888c3427ea52b758610c392efbe85eafe
XxPedroAlmeidaxX <pedrohenriquealmeidacosta@gmail.com>
2019-04-30 00:22:49 -0300

    Function to create client

---
+++ scripts.js
@@ -0,0 +1,30 @@
+class Client {
+    constructor(name, lastName, email, telephone, tags, picture, isFavorite, observations, type) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.telephone = telephone;
+        this.tags = tags;
+        this.picture = picture;
+        this.isFavorite = isFavorite;
+        this.observations = observations;
+        this.type = type;    
+    }
+}
+
+function createClient() {
+    name = document.getElementById("clientName").value;
+    lastName = document.getElementById("lastName").value;
+    email = document.getElementById("email").value;
+    telephone = document.getElementById("telephone").value;
+    //TODO change according with the final implementation
+    tags = ""
+    //TODO Implement Picture
+    picture = ""
+    //TODO change according with the final implementation
+    isFavorite = ""
+    observations = document.getElementById("observations").value;
+    type = "";
+
+    return new Client(name, lastName, email, telephone, tags, picture, isFavorite, observations, type)
+}
