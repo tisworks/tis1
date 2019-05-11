066b6355f7a5fcbae1487e314a4a76e127f1b232
Tomas Antonio Costa Campos <tomasantoniocampos@gmail.com>
2019-05-11 19:19:48 -0300

    Persistencia de dados em localStorage
    Funções para criar e armazenar dados dos clientes adastrados no localStorage do navegador

---
+++ static/data/clientes.js
@@ -0,0 +1,26 @@
+var cadastroDefault = 
+[
+    {
+        'name': 'NameTeste',
+        'lastName': 'LasNameTeste',
+        'email': 'testeteste@gmail.com',
+        'telephone': '12345678',
+        'tags': ['tagTeste1', 'tagTeste2'],
+        'picture': 'implementacaoFutura',
+        'isFavorite': 'true',
+        'observations': 'observacao Testeeeee',
+        'type': 'Cliente'
+    },
+
+    {
+        'name': 'NameTeste2',
+        'lastName': 'LasNameTeste2',
+        'email': 'testeteste2@gmail.com',
+        'telephone': '22345678',
+        'tags': ['tagTeste3', 'tagTeste4'],
+        'picture': 'implementacaoFutura2',
+        'isFavorite': 'false',
+        'observations': 'observacao Teste2',
+        'type': 'Cliente'
+    },
+]
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,11 @@
+var _clients = null;
+
+$(document).ready(function() {
+    carregarEventListeners();
+    carregarDadosSalvos();
+    //TODO: carregarDadosIniciais(); -- Iterar pelo objeto _clients e ir montando o html com os cards cadastrados
+});
+
 class Client {
     constructor(name, lastName, email, telephone, tags, picture, isFavorite, observations, type) {
         this.name = name;
@@ -21,8 +29,7 @@ function createClient() {
     tags = ""
     //TODO change according with the final implementation
     picture = ""
-    //TODO change according with the final implementation
-    isFavorite = ""
+    isFavorite = $('#favorite').is(':checked');
     observations = document.getElementById("observations").value;
     //TODO change according with the final implementation
     type = "";
@@ -30,6 +37,41 @@ function createClient() {
     return new Client(name, lastName, email, telephone, tags, picture, isFavorite, observations, type)
 }
 
-$('#addContact').on('shown.bs.modal', function () {
-    $('#inputBtn').trigger('focus')
-});
+function saveClientLocalStorage(client) {
+    var clientToSave = {
+        'name': client.name,
+        'lastName': client.lastName,
+        'email': client.email,
+        'telephone': client.telephone,
+        'tags': client.tags,
+        'picture': client.picture,
+        'isFavorite': client.isFavorite,
+        'observations': client.observations,
+        'type': client.type
+    }
+
+    _clients.push(clientToSave);
+    localStorage.setItem('clients', JSON.stringify(_clients));
+};
+
+function carregarEventListeners(){
+
+    $('#addContact').on('shown.bs.modal', function () {
+        $('#inputBtn').trigger('focus')
+    });
+
+    $('#confirmAdd').on('click', function(){
+        var newClient = createClient();
+        saveClientLocalStorage(newClient);
+    });
+}
+
+//carrega dados default caso não tenha nenhum dado salvo. 
+//Os dados default estão salvos num Json em: static/data/clientes.js
+function carregarDadosSalvos(){
+    if(!localStorage.clients)
+        _clients = cadastroDefault;
+    else
+        _clients = JSON.parse(localStorage.clients);
+}
+
