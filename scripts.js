95d47966b7780470bb8e6a05a3c27594870a2fb7
XxPedroAlmeidaxX <pedrohenriquealmeidacosta@gmail.com>
2019-05-12 18:55:20 -0300

    Layout dynamic implementation

--- scripts.js
+++ scripts.js
@@ -73,15 +73,81 @@ function loadFieldMasks() {
     $('#telephone').mask('(99) 99999-9999');
 }
 
+//TODO 
+//Realizar o upload de uma imagem real
+//Alterar layout de acordo com isFavorite
+//Inserir tags verdadeiras (provavel logica com loop para insercao das mesmas)
+//Melhorar modulacao desse metodo
+//Remover fake row do HTML
 function insertContactCard(contact) {
-    $("#cardPlace").append(
-        `<div class="row"><div class="col-12"><div class="card"> 
-            <div class="card-body">
-                <h5 class="card-title">` + contact.name + `</h5>
-                <p class="card-text">LastName: ` + contact.lastName + `</p>
-                <p class="card-text">Email: ` + contact.email + `</p>
-                <p class="card-text">Telephone: ` + contact.telephone + `</p>
-                <p class="card-text">observations: ` + contact.observations + `</p>
+    $("#cardPlace").append(`
+    <div id="cardPlace">
+        <div class="d-none d-xl-block">
+            <div class="row row-eq-height">
+                <div class="col-3">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <img src="assets/skull.png" alt="Imagem do Contato">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-9">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-10">
+                                    <h4 class="card-title">` + contact.name + " " + contact.lastName + `</h4>
+                                </div>
+                                <div class="col-1">
+                                    <button type="button" class="btn btn-info ava-btn">
+                                        <span class="sr-only">Favorito</span>
+                                        <i class="material-icons">grade</i>
+                                    </button>
+                                </div>
+                            </div>
+                            <h5 class="card-text "><b>Tel:</b>` + contact.telephone + `</h5>
+                            <h5 class="card-text "><b>Email:</b>` + contact.email + `</h5>
+                            <div class="row">
+                                <div class="col-3">
+                                    <span><h5>Cliente</h5></span>
+                                </div>
+                                <div class="col-3">
+                                    <span><h5>Tag1</h5></span>
+                                </div>
+                                <div class="col-3">
+                                    <span><h5>Tag2</h5></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div></div></div>`);
+        </div>
+
+        <div class="card d-xl-none">
+            <button type="button" class="btn btn-info ava-btn">
+                <span class="sr-only">Favorito</span>
+                <i class="material-icons">grade</i>
+            </button>
+            <div class="text-center">
+                <img class="card-img-top" src="assets/skull.png" alt="Imagem do Contato">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">` + contact.name + " " + contact.lastName + `</h5>
+                <h6 class="card-text "><b>Tel:</b>` + contact.telephone + `</h6>
+                <h6 class="card-text "><b>Email:</b>` + contact.email + `</h6>
+                <div class="row">
+                    <div class="col-4">
+                        <span><h6>Cliente</h6></span>
+                    </div>
+                    <div class="col-4">
+                        <span><h6>Tag1</h6></span>
+                    </div>
+                    <div class="col-4">
+                        <span><h6>Tag2</h6></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`);
 }
